04568ac1f9968683b527182b5a4aad79e691b4c7
liza <liza@DESKTOP-OST2SGM>
2024-11-19 13:51:52 +0200

    Add project files.

---
+++ Laboratory_7/Book.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratory_7
+{
+	public class Book : IComparable
+	{
+		public string Title { get; set; }
+		public string Author { get; set; }
+		public int NumPages { get; set; }
+		public int WordCount { get; set; }
+		public string Publisher { get; set; }
+		public int YearPublished { get; set; }
+		public string Language { get; set; }
+		public string Genre { get; set; }
+		public int Onthepage { get; set; }
+		public bool SizeofBook {  get; set; } 
+
+
+		public int CalculateWordsPerPage()
+		{
+			if (NumPages > 0)
+			{
+				return (int)WordCount / NumPages;
+			}
+			else
+			{
+				Console.WriteLine("Кількість сторінок не може бути 0 або менше.");
+				return 0;
+			}
+		}
+
+		public bool IsLargeBook()
+		{
+			if (NumPages >= 500)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+
+		public double WordsPerPage
+		{
+			get { return CalculateWordsPerPage(); }
+		}
+
+
+
+		public bool BigorSmall
+		{
+			get
+			{
+				return NumPages >= 500;
+			}
+		}
+
+
+	
+		public Book() { }
+
+
+		public Book(string title, string author, int numPages, int wordCount, string publisher, int yearPublished, 
+			string language, string genre, int onthepage, bool sizeofbook)
+		{
+			Title = title;
+			Author = author;
+			NumPages = numPages;
+			WordCount = wordCount;
+			Publisher = publisher;
+			YearPublished = yearPublished;
+			Language = language;
+			Genre = genre;
+			Onthepage = onthepage; 
+			SizeofBook = sizeofbook;
+		}
+
+		public string Info()
+		{
+			return Title + ", " + Author + ", " + NumPages;
+		}
+		public int CompareTo (object obj)
+		{
+			Book t = obj as Book;
+			return string.Compare(Title, t.Title);
+		}
+	}
+}
---
+++ Laboratory_7/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratory_7
+{
+    internal class Program
+    {
+        static List<Book> books;
+        static void PrintBooks ()
+        {
+            foreach (Book book in books) 
+            { 
+                Console.WriteLine (book.Info().Replace('i', 'i'));
+            }
+            Console.WriteLine ();
+        }
+        static void Main(string[] args)
+        {
+            books = new List<Book> ();
+            FileStream fs = new FileStream("Бінарні_дані.books", FileMode.Open);
+            BinaryReader reader = new BinaryReader(fs);
+            try
+            {
+                Book book;
+                Console.WriteLine("Читаємо дані з файлу... \n");
+                while (reader.BaseStream.Position < reader.BaseStream.Length)
+                {
+                    book = new Book();
+                    for (int i=1; i<=10; i++)
+                    {
+                        switch (i)
+                        {
+                            case 1:
+                                book.Title = reader.ReadString(); break;
+                            case 2:
+                                book.Author = reader.ReadString(); break;
+                            case 3:
+                                book.NumPages = reader.ReadInt32(); break;
+                            case 4:
+                                book.WordCount = reader.ReadInt32(); break;
+                            case 5:
+                                book.Publisher = reader.ReadString(); break;
+                            case 6:
+                                book.YearPublished = reader.ReadInt32(); break;
+                            case 7:
+                                book.Language = reader.ReadString(); break;
+                            case 8:
+                                book.Genre = reader.ReadString(); break;
+                            case 9:
+                                book.Onthepage = reader.ReadInt32(); break;
+                            case 10:
+                                book.SizeofBook = reader.ReadBoolean(); break;
+                        }
+                    }
+                    books.Add(book);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Сталась помилка: {0}", ex.Message);
+            }
+            finally { reader.Close(); }
+            Console.WriteLine("Несортований перелік книг: {0}", books.Count);
+            PrintBooks();
+            books.Sort();
+            Console.WriteLine("Сортований перелік книг: {0}", books.Count);
+            PrintBooks();
+            Console.WriteLine("Додаємо новий запис: Чума");
+            Book bookPlague = new Book("Чума", "Альбер Камю", 550, 250000, "French books", 1947,
+                "Французька", "Соціальний роман", 250, false);
+            books.Add(bookPlague);
+            books.Sort();
+            Console.WriteLine("Перелік книг: {0}", books.Count);
+            PrintBooks();
+            Console.WriteLine("Видаляємо останнє значення");
+            books.RemoveAt(books.Count - 1);
+            Console.WriteLine("Перелік книг: {0}", books.Count);
+            PrintBooks();
+            Console.ReadKey();
+        }
+
+    }
+}
